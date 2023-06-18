da227dd3e7136441c41a4d1640549db59269a709
Anne Edmondson <edmondson.anne@gmail.com>
2023-06-17 20:38:47 -0600

    Commit modified placeholders for cell values, partial JS code for displaying game result

--- index.js
+++ index.js
@@ -4,7 +4,67 @@
  - A button should be available to clear the grid and restart the game.
 - When a player has won, or the board is full and the game results in a draw, a Bootstrap alert or similar Bootstrap component should appear across the screen announcing the winner. */
 
-function displayValue(e) {
+/* function displayValue(e) {
     alert(`value: ${e.target.textContent}`)
     alert(`position: ${e.target.getAttribute("data-index")}`);
+  } */
+
+let playerX = 'x'
+let playerO = 'o'
+let winningCombos = [
+	[0, 1, 2],
+	[3, 4, 5],
+	[6, 7, 8],
+	[0, 3, 6],
+	[1, 4, 7],
+	[2, 5, 8],
+	[0, 4, 8],
+	[2, 4, 6]
+]
+
+let cellNumbers = document.querySelectorAll('[data-cell]')
+let gameBoard = document.querySelector('#ttt-container')
+let gameResult = document.querySelector('#ttt-result')
+let gameRestartBtn = document.querySelector('#gameRestartBtn')
+let resultMessage = document.querySelector('resultText')
+let isPlayerO = false
+
+playGame()
+
+gameRestartBtn.addEventListener('click', playGame)
+
+function playGame() {
+	isPlayerO = false
+	cellNumbers.forEach(cell => {
+		cell.classList.remove(playerX)
+		cell.classList.remove(playerO)
+		cell.removeEventListener('click', playerTurn)
+		cell.addEventListener('click', playerTurn, { once: true })
+	})
+	
+  setBoardHoverClass()
+	resultMessage.classList.remove('show')
+}
+
+function playerTurn(e) {
+	let cell = e.target
+	let currentPlayer = isPlayerO ? playerO : playerX
+	placeMark(cell, currentPlayer)
+	if (checkWin(currentPlayer)) {
+		endGame(false)
+	} else if (isDraw()) {
+		endGame(true)
+	} else {
+		swapTurns()
+		setBoardHoverClass()
+	}
+}
+
+function gameEnd (draw) {
+  if (draw) {
+    resultMessage.innerHTML = 'It is a tie!'
+  } else {
+    resultMessage.innerHTML = `${isPlayerO ? 'Xs win!' : 'Os win!'}`
   }
+  gameResult.classList.add('show')
+}
